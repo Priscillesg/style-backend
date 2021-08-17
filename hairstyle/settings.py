9b66e6701fe9296bcb73787e6a14d7f255621296
Priscille Gwet <ngomakak@gmail.com>
2021-08-17 10:17:44 -0500

    Added Cors-headers in settings to allow the front-end to communicate with the backend

--- hairstyle/settings.py
+++ hairstyle/settings.py
@@ -39,17 +39,26 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+  
+    "http://localhost:3000",
+    
 ]
 
 ROOT_URLCONF = 'hairstyle.urls'
