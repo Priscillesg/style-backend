faba1de5cd875905c477324b02b0e3d3d1906c0c
Priscille Gwet <ngomakak@gmail.com>
2021-08-17 12:26:07 -0500

    Some changes

--- hairstyle/settings.py
+++ hairstyle/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!46y*&tif$#i807q+h3s4_qo(ixpnk!9dh1(^_68y$715gz8j1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hairbypris.herokuapp.com']
 
 
 # Application definition
